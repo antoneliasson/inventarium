ff8276e93fb786c565b46eaa5e07e8455f96396b
Anton Eliasson <devel@antoneliasson.se>
2014-04-17 08:54:25 +0200

    Maven quickstart-projekt

---
+++ src/main/java/se/antoneliasson/inventarium/App.java
@@ -0,0 +1,13 @@
+package se.antoneliasson.inventarium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
