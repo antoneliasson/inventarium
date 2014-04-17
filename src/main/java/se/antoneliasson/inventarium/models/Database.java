b8786361834d7bea6b4246105de46beb9ee26b5b
Anton Eliasson <devel@antoneliasson.se>
2014-04-17 11:50:08 +0200

    Klistrat in lite kod i databasen

--- src/main/java/se/antoneliasson/inventarium/models/Database.java
+++ src/main/java/se/antoneliasson/inventarium/models/Database.java
@@ -1,13 +1,27 @@
 package se.antoneliasson.inventarium.models;
 
+import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Database {
+
+    private final Logger log;
+    private final Connection connection;
+
     public Database(String filename) {
-        
+        log = LogManager.getLogger();
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException ex) {
+            log.fatal(ex);
+            System.exit(1);
+        }
+        connection = null;
     }
-    
+
     public Map<String, String> lookupProduct(GTIN id) {
         return new HashMap<>();
     }
